78bd0c7bc8171ee069ca3a22755b3142de8ac8f9
Md.Afthad <md.afthad@eazyfin.com>
2021-09-29 17:19:08 +0530

    added 1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,8 +20,8 @@ class _MyAppState extends State<MyApp> {
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     await FlurryEvent.initialize(
-        androidKey: "QX7WZTNJ8468J6SR8DR3",
-        iosKey: "XCCWQH4MCD45JHSM4BYN",
+        androidKey: "XQRP9F8D9WMW9KTMSSKC3",
+        iosKey: "7F4D3BQBCD5YRSF42N47",
         enableLog: true);
     await FlurryEvent.setUserId("userId");
     await FlurryEvent.logEvent("eventName");
